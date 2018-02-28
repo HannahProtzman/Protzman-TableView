af4fc7fdaf826392525228522042cd36da45fc4e
student <s520515@ol10020m.local>
2018-02-27 19:11:55 -0600

    Initial Commit

---
+++ Protzman-TableViewTests/Protzman_TableViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  Protzman_TableViewTests.swift
+//  Protzman-TableViewTests
+//
+//  Created by Protzman,Hannah R on 2/27/18.
+//  Copyright © 2018 Protzman,Hannah R. All rights reserved.
+//
+
+import XCTest
+@testable import Protzman_TableView
+
+class Protzman_TableViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
